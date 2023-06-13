928b7d355f9bd27b9c4a1719d061977926989b2a
Joao Vitor <jvmelop@gmail.com>
2023-06-13 03:01:02 -0300

    navbar e logo

---
+++ web/src/components/BigLogo.tsx
@@ -0,0 +1,13 @@
+const BigIcon = () => {
+    return (
+        <div className="flex items-center justify-center gap-1">
+            <div className="flex relative items-center justify-center w-24 h-24 rounded-lg bg-black cursor-default">
+                <p className="text-pink-500 font-bold text-[80px]/[80px] mt-[-10px]">
+                    H
+                </p>
+            </div>
+            <h2 className="text-gray-800 text-7xl font-bold">opeShop</h2>
+        </div>
+    );
+};
+export default BigIcon;
---
+++ web/src/components/NavBar.tsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+
+const NavBar = () => {
+    const { session, handleLogout } = useContext(AuthContext);
+
+    return (
+        <nav className="flex bg-neutral-900 py-4 justify-between items-center fixed w-full">
+            <div className="text-pink-500 font-bold text-base ml-6">
+                Olá, {session?.email}
+            </div>
+            <div className="text-white font-bold text-xl absolute w-full text-center pointer-events-none">
+                Logo
+            </div>
+            <div
+                onClick={handleLogout}
+                className="py-2 px-4 cursor-pointer mr-10 hover:bg-pink-500 hover:border-pink-500 text-white w-30 h-10 border border-white rounded"
+            >
+                Sair
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
